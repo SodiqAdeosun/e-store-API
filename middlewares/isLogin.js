1d10cc8dbabad6f441ce50a2341005a9fed3215c
SodiqAdeosun <sodiqadeosun@yahoo.com>
2023-11-01 10:38:24 +0100

    update

--- middlewares/isLogin.js
+++ middlewares/isLogin.js
@@ -5,8 +5,10 @@ const verifyToken = require("../helper/verifytoken");
 const isLogin = (req,res,next) => {
 //get token from header
 const token = getTokenFromHeader(req);
+
 // verify token
 const decodedUser = verifyToken(token);
+
 //save the user into req obj
 req.userAuth = decodedUser.id;
 if (!decodedUser) {
